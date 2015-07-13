21175e62d510667eeabf7cbf06d10c5247f0a1ec
jeffplourd <jeffplourd@gmail.com>
2015-07-13 15:02:12 -0700

    Complete send request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,23 @@
 // YOUR CODE HERE:
 
+var app = {
+};
+
+app.init = function() {
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
